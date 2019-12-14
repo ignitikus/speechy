99b988d203ad7413c277caa5ed6d352244ef5b0d
NikolayK <nikolay.kim@codeimmersive.com>
2019-12-14 17:42:16 -0500

    creates liCreator.js conversation.js and speechToText.js

---
+++ conversation.js
@@ -0,0 +1,12 @@
+const conversation = [
+    {
+        text: 'Hello, I\'m a Chat-bot',
+        id: bot,
+    },
+    {
+        text: 'Hello Chat-bot',
+        id: user,
+    }
+]
+
+let currentConversation = [...conversation]
