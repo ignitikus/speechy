d576aeb22d6c3accc49a876d1e101f8587b1160f
Niko <nikolay.kim@codeimmersives.com>
2019-12-14 14:08:08 -0500

    adds speech recognition

---
+++ main.js
@@ -0,0 +1,32 @@
+const speech = new webkitSpeechRecognition() || speechRecognition();
+
+speech.continuous = false
+speech.interimResults = true
+speech.language = 'en-US'
+
+
+document.querySelector('.start-button').addEventListener('click', ()=>{
+    speech.start()
+})
+
+speech.onstart = ()=>{
+    recognizing = true
+}
+
+speech.onresult = (event)=>{
+    let interim_transcript = ''
+    let final_transcript = ''
+
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+        } else {
+            interim_transcript += event.results[i][0].transcript;
+        }
+    }
+    document.querySelector('.final').innerHTML = final_transcript;
+    document.querySelector('.interim').innerHTML = interim_transcript;
+    console.log(document.querySelector('.final').innerHTML)
+    console.log(document.querySelector('.interim').innerHTML)
+}
+
